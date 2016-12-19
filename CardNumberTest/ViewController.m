041df7240665e86c1a7fe619d603118fee75db4f
贾萍 <ping.jia@56qq.comgit config --global user.email ping.jia@56qq.com>
2016-12-19 11:23:35 +0800

    修复bug

--- CardNumberTest/ViewController.m
+++ CardNumberTest/ViewController.m
@@ -14,71 +14,50 @@
 //    NSArray* bankBin;
     
 }
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
 @end
 
 @implementation ViewController
+- (IBAction)transfer:(id)sender {
+    if(self.textField.text.length<=0 || self.textField.text.length<16 || self.textField.text.length>19) {
+        return;
+    }
+    if ([self returnBankName:self.textField.text].length>0) {
+        _resultLabel.text = [self returnBankName:self.textField.text];
+    }
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    [self returnBankName:@"你的银行卡号"];
+//    [self returnBankName:@"你的银行卡号"];
 }
 
 - (NSString *)returnBankName:(NSString*) idCard{
     
-    NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"bank" ofType:@"plist"];
-    NSDictionary* resultDic = [NSDictionary dictionaryWithContentsOfFile:plistPath];
-    NSArray *bankName = [resultDic objectForKey:@"bankName"];
-    NSArray *bankBin = [resultDic objectForKey:@"bankBin"];
-    
-    int index = -1;
-    
     if(idCard==nil || idCard.length<16 || idCard.length>19){
-        
+        _resultLabel.text = @"卡号不合法";
         return @"";
         
     }
+    NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"bank" ofType:@"plist"];
+    NSDictionary* resultDic = [NSDictionary dictionaryWithContentsOfFile:plistPath];
+    NSArray *bankBin = resultDic.allKeys;
     
     //6位Bin号
-    
     NSString* cardbin_6 = [idCard substringWithRange:NSMakeRange(0, 6)];
-    
-    for (int i = 0; i < bankBin.count; i++) {
-        
-        if ([cardbin_6 isEqualToString:bankBin[i]]) {
-            
-            index = i;
-            
-        }
-        
-    }
-    
-    if (index != -1) {
-        
-        return bankName[index];
-        
-    }
-    
     //8位Bin号
-    
     NSString* cardbin_8 = [idCard substringWithRange:NSMakeRange(0, 8)];
-    
-    for (int i = 0; i < bankBin.count; i++) {
-        
-        if ([cardbin_8 isEqualToString:bankBin[i]]) {
-            
-            index = i;
-            
-        }
-        
+
+    if ([bankBin containsObject:cardbin_6]) {
+        return [resultDic objectForKey:cardbin_6];
+    }else if ([bankBin containsObject:cardbin_8]){
+        return [resultDic objectForKey:cardbin_8];
+    }else{
+        _resultLabel.text = @"plist文件中不存在请自行添加对应卡种";
+        return @"";
     }
-    
-    if (index != -1) {
-        
-        return bankName[index];
-        
-    }
-    
     return @"";
     
 }
